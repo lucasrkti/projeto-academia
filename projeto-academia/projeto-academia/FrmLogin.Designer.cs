f6636b87283576ef3f890f65c605de58edfcf0f2
Lucas <lucas.ribeirosil@outlook.com>
2023-03-28 19:48:18 -0300

    refs #dev alterações no form

--- projeto-academia/projeto-academia/FrmLogin.Designer.cs
+++ projeto-academia/projeto-academia/FrmLogin.Designer.cs
@@ -28,19 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(82, 266);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Location = new System.Drawing.Point(57, 136);
+            this.lblUsuario.MaxLength = 15;
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(132, 23);
+            this.lblUsuario.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(60, 194);
+            this.txtSenha.MaxLength = 15;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(129, 23);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // C
+            // 
+            this.C.AutoSize = true;
+            this.C.Location = new System.Drawing.Point(53, 119);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(47, 15);
+            this.C.TabIndex = 3;
+            this.C.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(31, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Acesso | Academia Strong";
+            // 
+            // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 354);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(253, 319);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button btnLogin;
+        private TextBox lblUsuario;
+        private TextBox txtSenha;
+        private Label C;
+        private Label label2;
+        private Label label1;
     }
 }
